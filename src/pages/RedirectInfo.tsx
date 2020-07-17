5198db8acd242ca7f71ebb19d048107cf25700ad
Loeka1234 <loekalievens@hotmail.com>
2020-07-17 15:04:39 +0200

    Belongs to previous commit

--- src/pages/RedirectInfo.tsx
+++ src/pages/RedirectInfo.tsx
@@ -4,6 +4,10 @@ import { useParams } from "react-router-dom";
 
 // Styled components
 import FlexDiv from "../styledComponents/FlexDiv";
+import Loader from "../styledComponents/Loader";
+
+// Components
+import DisplayRedirect from "../components/DisplayRedirect";
 
 export interface Props {}
 
@@ -11,39 +15,36 @@ const RedirectInfo: React.FC<Props> = () => {
     const { redirect } = useParams();
     const [url, setUrl] = useState("");
     const [redirectsTo, setRedirectsTo] = useState("");
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
 
     useEffect(() => {
-        const getData = async () => {
-            const response = await axios.get(
-                process.env.REACT_APP_API_ENDPOINT + `/info/${redirect}`
-            );
-            setUrl(response.data.url);
-            setRedirectsTo(response.data.redirectsTo);
-        };
-        getData();
-    }, []);
+        axios
+            .get(process.env.REACT_APP_API_ENDPOINT + `/info/${redirect}`)
+            .then(response => {
+                setUrl(response.data.url);
+                setRedirectsTo(response.data.redirectsTo);
+                setLoading(false);
+            })
+            .catch(err => {
+                if (
+                    err.response &&
+                    (err.response.status === 400 || err.response.status === 500)
+                )
+                    setError(err.response.data.error);
+                else setError(err.message);
+            });
+    }, [redirect]);
 
     return (
-        <main>
-            <FlexDiv flexDir="column">
-                <h1>Redirect</h1>
-                <p>
-                    Url:{" "}
-                    <a href={url} target="_blank" rel="noopener noreferrer">
-                        {url.split("://")[1]}
-                    </a>
-                </p>
-                <p>
-                    Redirects to:{" "}
-                    <a
-                        href={redirectsTo}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        {redirectsTo.split("://")[1]}
-                    </a>
-                </p>
-            </FlexDiv>
+        <main style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+            {error ? (
+                <h1>{error}</h1>
+            ) : loading ? (
+                <Loader />
+            ) : (
+                <DisplayRedirect url={url} redirectsTo={redirectsTo} />
+            )}
         </main>
     );
 };

6b8e34a68d7c3b7a6960ad83c028da016200f6e6
Loeka1234 <loekalievens@hotmail.com>
2020-07-17 23:49:42 +0200

    Update LatestRedirects.tsx

--- src/components/LatestRedirects.tsx
+++ src/components/LatestRedirects.tsx
@@ -38,7 +38,7 @@ const LatestRedirects: React.SFC<Props> = ({ className }) => {
                     </tr>
                 </thead>
                 <tbody>
-                    {redirects.map((redirect, i) => (
+                    {redirects && redirects.map((redirect, i) => (
                         <tr key={i}>
                             <td>
                                 {new Date(
